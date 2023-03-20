b3aefaa0aca612d936a248ef3713f8e88aa57ae4
Karoline <shiamosan@gmail.com>
2023-03-19 23:44:48 -0300

    criação de eventos

--- fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class SegundoComponenteComponent {
   nome = "João";
   dataNascimento = "1995-01-01";
+  urlImagem = "/assets/o-que-e-paisagem.jpg";
+
+  mostrarDataNascimento(){
+    alert(`A data de nascimento de João é: ${this.dataNascimento}`);
+  }
 }
