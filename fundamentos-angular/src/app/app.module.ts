f1ee6f1429ebd92c2897f98d72b0eddb033d03db
Karoline <shiamosan@gmail.com>
2023-03-26 01:00:44 -0300

    ciclo de vida de um componente

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ComponentePaiComponent } from './componente-pai/componente-pai.componen
 import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
 import { ExemploServicoslComponent } from './exemplo-servicos1/exemplo-servicosl.component';
 import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2.component';
+import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { CicloDeVidaPaiComponent } from './ciclo-de-vida-pai/ciclo-de-vida-pai.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { ExemploServicos2Component } from './exemplo-servicos2/exemplo-servicos2
     ComponentePaiComponent,
     ComponenteFilhoComponent,
     ExemploServicoslComponent,
-    ExemploServicos2Component
+    ExemploServicos2Component,
+    CicloDeVidaComponent,
+    CicloDeVidaPaiComponent
   ],
   imports: [
     BrowserModule,
