76b11074cfd5d859a840f2a0b9a16f79be1031e7
Karoline <shiamosan@gmail.com>
2023-04-06 22:08:16 -0300

    como redirecionar para erro 404

--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
